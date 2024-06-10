a16a5be0a97d9fa73e7239c2973a2da40a0adc50
Lucas Gomes <lucaass1997rp@gmail.com>
2024-06-10 15:12:34 -0300

    feat: modifying mongoose.database.js

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.zd9do5a.mongodb.net/?retryWrites=true&w=majority&appName=FscTaskManagerCluster`,
-            { useNewUrlParser: true, useUnifiedTopology: true }
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fsctaskmanagercluster.zd9do5a.mongodb.net/?retryWrites=true&w=majority&appName=FscTaskManagerCluster`
         );
         console.log("Connected to MongoDB!");
     } catch (err) {
